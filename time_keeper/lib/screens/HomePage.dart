5391aea7913f1001bbfecfbacec731101b66da66
Jimit Shah <jimit24365@gmail.com>
2019-12-24 22:03:02 +0530

    Making transparent statusbar

--- time_keeper/lib/screens/HomePage.dart
+++ time_keeper/lib/screens/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,47 +11,51 @@ class HomePage extends StatefulWidget {
 class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          backgroundColor: Colors.white,
-          body: SafeArea(
+      theme: ThemeData(primaryColor: Colors.white,
+      accentColor:  Colors.white),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
             child: CustomScrollView(
-              slivers: <Widget>[
-                SliverAppBar(
-                  backgroundColor: Colors.white,
-                  leading: Icon(
-                    Icons.access_time,
-                    color: Colors.grey,
-                  ),
-                  floating: true,
-                  title: Text(
-                    "TimeKeeper",
-                    style: TextStyle(color: Colors.grey),
-                  ),
-                  expandedHeight: 50,
-                  elevation: 5.0,
-                ),
-                SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                    (context, index) => ListTile(title: Text('Item #$index')),
-                    childCount: 1000,
-                  ),
-                ),
-              ],
+          slivers: <Widget>[
+            SliverAppBar(
+              backgroundColor: Colors.white,
+              leading: Icon(
+                Icons.access_time,
+                color: Colors.grey,
+              ),
+              floating: true,
+              title: Text(
+                "TimeKeeper",
+                style: TextStyle(color: Colors.grey),
+              ),
+              expandedHeight: 50,
+              elevation: 5.0,
             ),
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-            items: [
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.av_timer), title: Text("Timer")),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.pie_chart), title: Text("Records")),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.calendar_today), title: Text("Calendar"))
-            ],
-          ),
-        ));
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) => ListTile(title: Text('Item #$index')),
+                childCount: 1000,
+              ),
+            ),
+          ],
+        )),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.av_timer), title: Text("Timer")),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.pie_chart), title: Text("Records")),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.calendar_today), title: Text("Calendar"))
+          ],
+        ),
+      ),
+    );
   }
 }
 
